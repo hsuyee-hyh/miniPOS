cc4cc47e653c430f73a5da71bb389d05e4c8e48d
hsuyee <hsuyee.hyh.01@gmail.com>
2025-10-16 11:27:11 +0630

    [Added] Add customer info and created person on UI

--- reactapp1.client/src/Invoice/ShowInvoice.jsx
+++ reactapp1.client/src/Invoice/ShowInvoice.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import Navbar from "../Layout/Navbar";
 import { useEffect, useState } from "react";
-import { Col, message, Row, Spin } from "antd";
+import { Alert, Col, message, Row, Spin } from "antd";
 
 export default function ShowInvoice() {
   const { customerId, invoiceId } = useParams();
@@ -11,6 +11,41 @@ export default function ShowInvoice() {
   const [invoice, setInvoice] = useState(null);
   const [loading, setLoading] = useState(false);
   const [orderItems, setOrderItems] = useState([]);
+  const [customer, setCustomer] = useState(null);
+
+  useEffect(() => {
+    if (errorMsg) {
+      const timer = setTimeout(() => {
+        setErrorMsg(null);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [errorMsg]);
+
+  // fetch customer info
+  useEffect(() => {
+    const fetchCustomer = async () => {
+      try {
+        const customerResponse = await fetch(
+          `https://localhost:7299/api/customer?id=${customerId}`
+        );
+        const customerResponseData = await customerResponse.json();
+
+        if (customerResponseData.error) {
+          setErrorMsg(customerResponseData.error);
+          return;
+        }
+
+        if (customerResponseData.success) {
+          setCustomer(customerResponseData.customer);
+        }
+      } catch (err) {
+        setErrorMsg("Something went wrong while retreiving Customer Infos.");
+        console.error("Error fetching customer data: ", err);
+      }
+    };
+    fetchCustomer();
+  }, [customerId]);
 
   // fetch invoice & order-item
   useEffect(() => {
@@ -73,25 +108,54 @@ export default function ShowInvoice() {
 
   return (
     <>
-      {console.log("customer id from invoice page: ", customerId)}
-      {console.log("invoice id from invoice page: ", invoiceId)}
-
       <Navbar />
       <div className="p-6">
-        <h2 className="text-xl font-semibold">Invoice Details </h2>
-        <div className="text-sm">({invoiceId})</div>
-        <div className="mt-4">
-          <Row type="flex">
-            <Col span={4} className="font-bold border border-gray-300 p-3">
+        <div className=" flex-col items-center justify-center">
+          {errorMsg && (
+            <Alert message={errorMsg} type="error" showIcon className="mb-4" />
+          )}
+          <h2 className="text-xl font-semibold text-start">Invoice Details </h2>
+
+          <div className="text-sm">({invoiceId})</div>
+          <span className="self-end">
+            Customer Code: {customer.customerCode}
+          </span>
+          <span className="self-end">Customer: {customer.name}</span>
+          {invoice && (
+            <span className="self-end">Created by: {invoice[0].createdBy}</span>
+          )}
+
+          <Row justify="center" align="middle" className="mt-4">
+            <Col
+              xs={4}
+              sm={4}
+              md={6}
+              className="font-bold border border-gray-300 p-3"
+            >
               Product
             </Col>
-            <Col span={4} className="font-bold border border-gray-300 p-3">
+            <Col
+              xs={4}
+              sm={4}
+              md={6}
+              className="font-bold border border-gray-300 p-3"
+            >
               Total Selling Price
             </Col>
-            <Col span={4} className="font-bold border border-gray-300 p-3">
+            <Col
+              xs={4}
+              sm={4}
+              md={6}
+              className="font-bold border border-gray-300 p-3"
+            >
               Quantity
             </Col>
-            <Col span={4} className="font-bold border border-gray-300 p-3">
+            <Col
+              xs={4}
+              sm={4}
+              md={6}
+              className="font-bold border border-gray-300 p-3"
+            >
               Balance
             </Col>
           </Row>
@@ -103,17 +167,37 @@ export default function ShowInvoice() {
               );
               return relatedOrderItems.map((i) => (
                 <>
-                  <Row key={inv.id} type="flex">
-                    <Col span={4} className=" border border-gray-300 p-3">
+                  <Row key={inv.id} justify="center" align="start">
+                    <Col
+                      xs={4}
+                      sm={4}
+                      md={6}
+                      className=" border border-gray-300 p-3"
+                    >
                       {i.product}
                     </Col>
-                    <Col span={4} className=" border border-gray-300 p-3">
+                    <Col
+                      xs={4}
+                      sm={4}
+                      md={6}
+                      className=" border border-gray-300 p-3"
+                    >
                       {i.totalSellingCost}
                     </Col>
-                    <Col span={4} className=" border border-gray-300 p-3">
+                    <Col
+                      xs={4}
+                      sm={4}
+                      md={6}
+                      className=" border border-gray-300 p-3"
+                    >
                       {i.quantity}
                     </Col>
-                    <Col span={4} className=" border border-gray-300 p-3">
+                    <Col
+                      xs={4}
+                      sm={4}
+                      md={6}
+                      className=" border border-gray-300 p-3"
+                    >
                       {i.balance}
                     </Col>
                   </Row>
@@ -126,35 +210,65 @@ export default function ShowInvoice() {
 
           {invoice && invoice.length > 0 && (
             <div>
-              <Row key={invoice[0].invoiceId} type="flex">
-                <Col span={4}></Col>
-                <Col span={4}></Col>
-                <Col span={4} className="border border-gray-300 p-3">
+              <Row key={invoice[0].invoiceId} justify="center" align="start">
+                <Col xs={4} sm={4} md={6}></Col>
+                <Col xs={4} sm={4} md={6}></Col>
+                <Col
+                  xs={4}
+                  sm={4}
+                  md={6}
+                  className="border border-gray-300 p-3"
+                >
                   Total Balance
                 </Col>
-                <Col span={4} className="border border-gray-300 p-3">
+                <Col
+                  xs={4}
+                  sm={4}
+                  md={6}
+                  className="border border-gray-300 p-3"
+                >
                   {invoice[0].totalBalance}
                 </Col>
               </Row>
 
-              <Row key={invoice[0].invoiceId} type="flex">
-                <Col span={4}></Col>
-                <Col span={4}></Col>
-                <Col span={4} className="border border-gray-300 p-3">
+              <Row key={invoice[0].invoiceId} justify="center" align="start">
+                <Col xs={4} sm={4} md={6}></Col>
+                <Col xs={4} sm={4} md={6}></Col>
+                <Col
+                  xs={4}
+                  sm={4}
+                  md={6}
+                  className="border border-gray-300 p-3"
+                >
                   <div className="font-semibold">Paid Amount</div>
                 </Col>
-                <Col span={4} className="border border-gray-300 p-3">
+                <Col
+                  xs={4}
+                  sm={4}
+                  md={6}
+                  className="border border-gray-300 p-3"
+                >
                   <div className="font-semibold">{invoice[0].paidAmount}</div>
                 </Col>
               </Row>
 
-              <Row key={invoice[0].invoiceId} type="flex">
-                <Col span={4}></Col>
-                <Col span={4}></Col>
-                <Col span={4} className="border border-gray-300 p-3">
+              <Row key={invoice[0].invoiceId} justify="center" align="start">
+                <Col xs={4} sm={4} md={6}></Col>
+                <Col xs={4} sm={4} md={6}></Col>
+                <Col
+                  xs={4}
+                  sm={4}
+                  md={6}
+                  className="border border-gray-300 p-3"
+                >
                   <div className="font-semibold">Left Balance</div>
                 </Col>
-                <Col span={4} className="border border-gray-300 p-3">
+                <Col
+                  xs={4}
+                  sm={4}
+                  md={6}
+                  className="border border-gray-300 p-3"
+                >
                   <div className="font-semibold text-red-500">
                     {invoice[0].remainingBalance}
                   </div>
